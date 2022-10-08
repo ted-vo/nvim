6509e16b3510b746275149f39f4fd87c42e14be9
ted-vo <V9N@tedvo.local>
2022-10-09 03:37:35 +0700

    feat: update with neovim-basic-config v0.8

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -9,27 +9,42 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
--- Remove statusline and tabline when in Alpha
-vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = { "AlphaReady" },
-  callback = function()
-    vim.cmd [[
-      set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-      set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
-    ]]
-  end,
-})
-
--- Set wrap and spell in markdown and gitcommit
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
+	pattern = { "gitcommit"},
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
 
-vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--   callback = function()
+--     vim.cmd [[
+--       if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
+--     ]]
+--   end,
+-- })
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	callback = function()
+		vim.cmd("tabdo wincmd =")
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+	callback = function()
+		vim.cmd("quit")
+	end,
+})
 
 -- Fixes Autocomment
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
@@ -45,9 +60,16 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   end,
 })
 
-vim.cmd [[
-  augroup _lsp
-    autocmd!
-    autocmd BufWritePre * lua vim.lsp.buf.formatting()
-  augroup endk
-]]
+-- Auto format after written
+vim.api.nvim_create_autocmd({ "BufWritePre"}, {
+  callback = function ()
+    vim.lsp.buf.formatting()
+  end
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	pattern = { "*.go" },
+	callback = function()
+		vim.lsp.codelens.refresh()
+	end,
+})
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,8 +4,10 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
+  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+	-- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
@@ -14,4 +16,8 @@ configs.setup({
 		enable = true,
 	},
 	indent = { enable = true, disable = { "python", "css" } },
+  context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
 })
