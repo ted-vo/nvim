fe1141baab80b3022c0821dc1ff125e832912c40
ted-vo <V9N@tedvo.dev>
2023-07-18 11:08:32 +0700

    feat: dap

---
+++ lua/user/dap.lua
@@ -0,0 +1,135 @@
+local M = {
+	"mfussenegger/nvim-dap",
+	commit = "6b12294a57001d994022df8acbe2ef7327d30587",
+	event = "VeryLazy",
+	dependencies = {
+		{
+			"rcarriga/nvim-dap-ui",
+		},
+		{
+			"theHamsta/nvim-dap-virtual-text",
+		},
+		{
+			"ravenxrz/DAPInstall.nvim",
+			commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de",
+			config = function()
+				require("dap-install").setup({})
+				require("dap-install").config("python", {})
+			end,
+		},
+	},
+}
+
+function M.config()
+	local dap = require("dap")
+
+	local dap_ui_status_ok, dapui = pcall(require, "dapui")
+	if not dap_ui_status_ok then
+		return
+	end
+
+	dap.listeners.after.event_initialized["dapui_config"] = function()
+		dapui.open()
+	end
+
+	dap.listeners.before.event_terminated["dapui_config"] = function()
+		dapui.close()
+	end
+
+	dap.listeners.before.event_exited["dapui_config"] = function()
+		dapui.close()
+	end
+
+	dapui.setup({
+		-- Expand lines larger than the window
+		expand_lines = true,
+		-- Layouts define sections of the screen to place windows.
+		-- The position can be "left", "right", "top" or "bottom".
+		-- The size specifies the height/width depending on position. It can be an Int
+		-- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+		-- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+		-- Elements are the elements shown in the layout (in order).
+		-- Layouts are opened in order so that earlier layouts take priority in window sizing.
+		layouts = {
+			{
+				elements = {
+					-- Elements can be strings or table with id and size keys.
+					{ id = "scopes", size = 0.25 },
+					"breakpoints",
+					"stacks",
+					"watches",
+				},
+				size = 40, -- 40 columns
+				position = "left",
+			},
+			{
+				elements = {
+					"repl",
+					"console",
+				},
+				size = 0.25, -- 25% of total lines
+				position = "bottom",
+			},
+		},
+		controls = {
+			-- Requires Neovim nightly (or 0.8 when released)
+			enabled = true,
+			-- Display controls in this element
+			element = "repl",
+			icons = {
+				pause = "",
+				play = "",
+				step_into = "",
+				step_over = "",
+				step_out = "",
+				step_back = "",
+				run_last = "",
+				terminate = "",
+			},
+		},
+		floating = {
+			max_height = nil, -- These can be integers or a float between 0 and 1.
+			max_width = nil, -- Floats will be treated as percentage of your screen.
+			border = "single", -- Border style. Can be "single", "double" or "rounded"
+			mappings = {
+				close = { "q", "<Esc>" },
+			},
+		},
+		windows = { indent = 1 },
+		render = {
+			max_type_length = nil, -- Can be integer or nil.
+			max_value_lines = 100, -- Can be integer or nil.
+		},
+	})
+
+	dap.adapters.codelldb = {
+		type = "server",
+		port = "${port}",
+		executable = {
+			-- provide the absolute path for `codelldb` command if not using the one installed using `mason.nvim`
+			command = "codelldb",
+			args = { "--port", "${port}" },
+			-- On windows you may have to uncomment this:
+			-- detached = false,
+		},
+	}
+	dap.configurations.c = {
+		{
+			name = "Launch file",
+			type = "codelldb",
+			request = "launch",
+			program = function()
+				local path
+				vim.ui.input({ prompt = "Path to executable: ", default = vim.loop.cwd() .. "/build/" }, function(input)
+					path = input
+				end)
+				vim.cmd([[redraw]])
+				return path
+			end,
+			cwd = "${workspaceFolder}",
+			stopOnEntry = false,
+		},
+	}
+end
+
+return M
