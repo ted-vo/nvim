01b6aa8b5950906bfc682b5f76ae1091f276e6c3
whoami <whoami@anonymous.org>
2024-06-07 14:39:30 +0700

    add keymap delete buffer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- buffers
+vim.keymap.set("n", "<S-q>", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
