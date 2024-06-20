738494cc36ab7242a0d5e7f44175a94335e1105b
ted-vo <vtnhon.dk@gmail.com>
2024-06-20 17:19:22 +0700

    conform: update

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,6 +3,8 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
+    go = { "goimports", "gofmt" },
+    php = { "pretty-php" },
   },
 
   format_on_save = {
