e892f34eb44d95cf52054bbee6fd6a3f0b0c340e
ted-vo <vtnhon.dk@gmail.com>
2024-08-19 10:11:30 +0700

    plugin: add minimap

---
+++ lua/plugins/minimap.lua
@@ -0,0 +1,33 @@
+return {
+  ---@module "neominimap.config.meta"
+  {
+    "Isrothy/neominimap.nvim",
+    enabled = true,
+    lazy = false, -- NOTE: NO NEED to Lazy load
+    -- Optional
+    keys = {
+      { "<leader>mt", "<cmd>Neominimap toggle<cr>", desc = "Toggle minimap" },
+      { "<leader>mo", "<cmd>Neominimap on<cr>", desc = "Enable minimap" },
+      { "<leader>mc", "<cmd>Neominimap off<cr>", desc = "Disable minimap" },
+      { "<leader>mf", "<cmd>Neominimap focus<cr>", desc = "Focus on minimap" },
+      { "<leader>mu", "<cmd>Neominimap unfocus<cr>", desc = "Unfocus minimap" },
+      { "<leader>ms", "<cmd>Neominimap toggleFocus<cr>", desc = "Toggle focus on minimap" },
+      { "<leader>mwt", "<cmd>Neominimap winToggle<cr>", desc = "Toggle minimap for current window" },
+      { "<leader>mwr", "<cmd>Neominimap winRefresh<cr>", desc = "Refresh minimap for current window" },
+      { "<leader>mwo", "<cmd>Neominimap winOn<cr>", desc = "Enable minimap for current window" },
+      { "<leader>mwc", "<cmd>Neominimap winOff<cr>", desc = "Disable minimap for current window" },
+      { "<leader>mbt", "<cmd>Neominimap bufToggle<cr>", desc = "Toggle minimap for current buffer" },
+      { "<leader>mbr", "<cmd>Neominimap bufRefresh<cr>", desc = "Refresh minimap for current buffer" },
+      { "<leader>mbo", "<cmd>Neominimap bufOn<cr>", desc = "Enable minimap for current buffer" },
+      { "<leader>mbc", "<cmd>Neominimap bufOff<cr>", desc = "Disable minimap for current buffer" },
+    },
+    init = function()
+      vim.opt.wrap = false -- Recommended
+      vim.opt.sidescrolloff = 36 -- It's recommended to set a large value
+      ---@type Neominimap.UserConfig
+      vim.g.neominimap = {
+        auto_enable = true,
+      }
+    end,
+  },
+}
