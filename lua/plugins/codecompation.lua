5da1a083eaafa25a75eadfb21165b7e1c516901a
whoami <whoami@anonymous.org>
2024-08-27 16:17:14 +0700

    plugin: codecompanion AI

---
+++ lua/plugins/codecompation.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- Optional
+      {
+        "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+        opts = {},
+      },
+    },
+    keys = {
+      { "ca", "<CMD>CodeCompanionAction<CR>", { desc = "CodeCompanionAction" } },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "ollama",
+        },
+        inline = {
+          adapter = "ollama",
+        },
+        agent = {
+          adapter = "ollama",
+        },
+      },
+      -- adapters = {
+      --   ollama = function()
+      --     return require("codecompanion.adapters").extend("ollama", {
+      --       env = {
+      --         url = "127.0.0.1:11434",
+      --       },
+      --     })
+      --   end,
+      -- },
+    },
+    config = function(_, opts)
+      require("codecompanion").setup(opts)
+      local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+
+      vim.api.nvim_create_autocmd({ "User" }, {
+        pattern = "CodeCompanionInline*",
+        group = group,
+        callback = function(request)
+          if request.match == "CodeCompanionInlineFinished" then
+            -- Format the buffer after the inline request has completed
+            require("conform").format { bufnr = request.buf }
+          end
+        end,
+      })
+    end,
+  },
+}
