ebf7640ccdd0616751e71ebb49fe80fafce0e94f
whoami <whoami@anonymous.org>
2024-09-21 11:19:14 +0700

    plugin: mason config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -52,6 +52,59 @@ M.ui = {
       border = "single",
     },
   },
+
+  mason = {
+    cmd = true,
+    pkgs = {
+      -- general
+      "masksman",
+      "prettier",
+
+      -- lua stuff
+      "lua-language-server",
+      "stylua",
+
+      -- web dev stuff
+      "css-lsp",
+      "html-lsp",
+      "typescript-language-server",
+      "tailwindcss-language-server",
+
+      -- c/cpp stuff
+      "clangd",
+      "clang-format",
+      "cmakelang",
+
+      -- zig
+      "zls",
+
+      -- go stuff
+      "gopls",
+      "goimports",
+      "golangci-lint-langserver",
+
+      -- shell stuff
+      "bash-language-server",
+      "shfmt",
+
+      -- config files stuff
+      "yaml-language-server",
+
+      -- helm
+      "helm-ls",
+
+      -- terraform
+      "terraform-ls",
+
+      -- python
+      "python-lsp-server",
+      "autoflake",
+      "pyflakes",
+
+      -- sql
+      "sqlls",
+    },
+  },
 }
 
 return M
