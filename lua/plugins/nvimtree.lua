73c681c19201c7d8674a782e38120b9485be5e1c
ted-vo <vtnhon.dk@gmail.com>
2025-03-22 13:44:05 +0700

    config: nvimtree increase width

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -5,6 +5,7 @@ return {
       local conf = require "nvchad.configs.nvimtree"
       conf.filters.dotfiles = true
       conf.renderer.root_folder_label = ":~:s?$?/..?"
+      conf.view.width = 50
 
       return conf
     end,
